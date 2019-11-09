7ea06474c62d32f9b0f444a36bea9b424c20f2c1
lleviy <ksetagonist27@gmail.com>
2019-11-09 22:45:24 +0300

    Silk Bookmark v.2.0

--- silk_bookmark/settings.py
+++ silk_bookmark/settings.py
@@ -11,36 +11,34 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import django_heroku
 import os
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-import json
-from django.core.exceptions import ImproperlyConfigured
-
-key = os.path.join(BASE_DIR, "secrets.json")
-with open(key) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secret=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+def get_secret(setting):
+    """Возвращает значение setting из файла secrets.json, прописанного в .gitignore"""
+    if os.path.exists('secrets.json'):
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        key = os.path.join(BASE_DIR, "secrets.json")
+        with open(key) as f:
+            secrets = json.loads(f.read())
+        try:
+            return secrets[setting]
+        except KeyError:
+            error_msg = "Set the {} environment variable".format(setting)
+            raise ImproperlyConfigured(error_msg)
+    else:
+        return os.environ[setting]
 
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,11 +46,35 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     # Сторонние приложения
     'bootstrap3',
+    'account',
+    'pinax_theme_bootstrap',
+    'bootstrapform',
     # Мои приложения
     'silk_bookmarks',
-    'users',
+    'unsplash_search',
+]
+
+STATICFILES_FINDERS = (           
+    'django.contrib.staticfiles.finders.FileSystemFinder',   
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+#Настройки почтового хоста
+SITE_ID = 1
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'silk.bookmark@gmail.com'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = get_secret("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'silk.bookmark@gmail.com'
+THEME_CONTACT_EMAIL = 'silk.bookmark@gmail.com'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.auth_backends.EmailAuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -85,10 +107,17 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #Для аккаунта
+                'account.context_processors.account',
+                'django.template.context_processors.request',
+                'pinax_theme_bootstrap.context_processors.theme',
             ],
         },
     },
 ]
+
+ACCOUNT_EMAIL_UNIQUE = True
+ACCOUNT_EMAIL_CONFIRMATION_REQUIRED = True
 
 WSGI_APPLICATION = 'silk_bookmark.wsgi.application'
 
@@ -122,27 +151,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
 
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-# Мои настройки
-LOGIN_URL = '/users/login/'
+# Страница входа по умолчанию
+LOGIN_URL = '/account/login/'
 
 # Настройки django-bootstrap3
 BOOTSTRAP3 = {
@@ -151,23 +166,19 @@ BOOTSTRAP3 = {
 
 #Настройки Heroku
 import dj_database_url
+db_config = dj_database_url.config()
+if db_config:
+    DATABASES['default'] =  db_config
 
 # Поддержка заголовка 'X-Forwarded-Proto' для request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['silk-bookmark.herokuapp.com/']
 
-# # Конфигурация статических ресурсов
+# Конфигурация статических ресурсов
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
--- silk_bookmarks/forms.py
+++ silk_bookmarks/forms.py
@@ -5,8 +5,11 @@ from .models import Topic, Entry
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
-        fields = ['text', 'author']
-        labels = {'text': 'title', 'author': 'author'}
+        fields = ['text', 'author', 'status', 'assoc', 'adv', 'photo_url']
+        labels = {'text': 'title', 'author': 'author', 'status': 'status', 'adv': '* your thoughts and feelings', 
+        'assoc': '* associations: several keywords, may be mood or genre', 'photo_url': '* photo url' ,
+        }
+        widgets = {'photo_url' : forms.HiddenInput()}
 
 class EntryForm(forms.ModelForm):
     class Meta:
@@ -14,3 +17,4 @@ class EntryForm(forms.ModelForm):
         fields = ['text']
         labels = {'text': ''}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+
